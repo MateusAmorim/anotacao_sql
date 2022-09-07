5d2c703bfa395512902094461803d690abc63989
MateusAmorim <83793612+MateusAmorim@users.noreply.github.com>
2022-09-07 20:42:29 -0300

    Update anotacao.sql

--- anotacao.sql
+++ anotacao.sql
@@ -1,7 +1,7 @@
---SELECT * FROM pessoas;
---SELECT * FROM pessoas WHERE sigla_estado="SP";
---SELECT * FROM pessoas WHERE sigla_estado<>"SP";
---SELECT * FROM pessoas WHERE qte_pontos=90;
+--SELECT * FROM pessoas; --mostra todos os dados da tabela pessoas.
+--SELECT * FROM pessoas WHERE sigla_estado="SP"; --mostra todos os dados da tabela pessoas quando a sigla do estado for = "SP".
+--SELECT * FROM pessoas WHERE sigla_estado<>"SP"; --mostra todos os dados da tabela pessoas quando a sigla do estado for diferente de "SP".
+--SELECT * FROM pessoas WHERE qte_pontos=90; --mostra todos os dados da tabela pessoas quando qte_pontos=90.
 --SELECT primeiro_nome, ultimo_nome FROM pessoas WHERE sigla_estado="SP" AND camiseta_ou_chapeu="camiseta"; --mais de uma condição.
 --SELECT primeiro_nome, ultimo_nome FROM pessoas WHERE sigla_estado="SP" AND camiseta_ou_chapeu="camiseta" AND nome_time="azul";
 --SELECT primeiro_nome, ultimo_nome, sigla_estado FROM pessoas WHERE sigla_estado="SP" OR sigla_estado="MG"; --quando uma condição ou outra é verdadeira.
@@ -17,8 +17,8 @@
 --SELECT primeiro_nome, ultimo_nome, qte_pontos from pessoas ORDER by primeiro_nome DESC; --ordenando de forma descendente.
 --SELECT primeiro_nome, ultimo_nome, qte_pontos from pessoas ORDER by primeiro_nome DESC, ultimo_nome ASC; --ordenando uma coluna ascendente e outra descendente.
 --SELECT primeiro_nome, ultimo_nome, qte_pontos from pessoas ORDER by qte_pontos DESC;
---select primeiro_nome from pessoas;
---select primeiro_nome, length(primeiro_nome)from pessoas;  --identifica quantidade de caracteres
+--select primeiro_nome from pessoas; --seleciona todos os primeiros nomes da tabela de pessoas.
+--select primeiro_nome, length(primeiro_nome)from pessoas;  --identifica quantidade de caracteres.
 --select DISTINCT(primeiro_nome) from pessoas order by primeiro_nome;  --identifica a quantidade de nomes distintos que existe na tabela de pessoas.
 --select count(*) FROM pessoas WHERE sigla_estado="SP"; --seleciona a quantidade de pessoas que tem a sigla_estado="SP".
 --SELECT primeiro_nome, ultimo_nome, pessoas.sigla_estado, nome_estado FROM pessoas INNER JOIN estados ON  pessoas.sigla_estado = estados.sigla_estado; --Ligando tabela pessoas com tabela estados.
@@ -32,7 +32,7 @@
 --SELECT sigla_estado, min(qte_pontos) from pessoas group by sigla_estado; --"MIN" retorna o menor valor de um grupo, ignorando os nulos.
 --SELECT sigla_estado, max(qte_pontos) from pessoas group by sigla_estado; --"MAX" retorna o maior valor dentro de cada grupo, ignorando nulos.
 --SELECT sigla_estado, sum(qte_pontos) from pessoas group by sigla_estado; --"SUM" soma todos os valores de um grupo, ignorando nulos.
---SELECT qte_pontos+10 from pessoas where qte_pontos>70
+--SELECT qte_pontos+10 from pessoas where qte_pontos>70 --seleciona qte_pontos adicionando 10 quando qte_pontos for maior que 70.
 --SELECT primeiro_nome, ultimo_nome, qte_pontos from pessoas where qte_pontos>= (select max(qte_pontos) from pessoas); --quantidade maxima de pontos da tabela de pessoas.
 --SELECT lower(primeiro_nome), upper(ultimo_nome) from pessoas; --primeiro nome em letras minusculas e ultimo nome em letras maiusculas.
 --SELECT lower(primeiro_nome), substr(ultimo_nome,1,5) from pessoas; --separa os 5 primeiros caracteres do ultimo nome.
@@ -46,3 +46,16 @@
 --INSERT INTO pessoas(primeiro_nome,ultimo_nome) VALUES("Dani", "Monteiro") --insere dados em tabela.
 --UPDATE pessoas SET qte_pontos=80 WHERE primeiro_nome="Danielle"; --"UPDATE" atualiza valores.
 --DELETE FROM pessoas WHERE primeiro_nome="Pedro"; --"DELETE" exclui dados da tabela.
+--show table status; --mostra a estrutura do banco de dados.
+select
+	ID_PILOTO,
+    NM_PILOTO,
+    case
+		when DT_NASCIMENTO is null then now()
+        else DT_NASCIMENTO
+	end 'DT_NASCIMENTO',
+    ID_PAIS,
+    ID_EQUIPE
+from 
+	tb_piloto; --substitui valor de data nulo por data atual.
+  
