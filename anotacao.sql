e6f058a5d6d42c6b55bfd1f417b357fb1dad059f
MateusAmorim <83793612+MateusAmorim@users.noreply.github.com>
2023-02-09 23:27:14 -0300

    Update anotacao.sql

--- anotacao.sql
+++ anotacao.sql
@@ -290,3 +290,15 @@ join customer cus using(customer_id)
 group by customer_id
 order by Total desc;
 
+select 
+	cus.customer_id as ID,
+    cus.first_name as Nome,
+    cus.last_name as Sobrenome,
+    sum(amount) as Total,
+    count(amount) as Compras
+from payment py
+join customer cus using(customer_id)
+group by customer_id
+having Total >= 150 and Compras >= 35
+order by Total desc;
+
